5fb6c10fda1aa6198a4fb6cc98f96dafbca6194e
Rohan1810 <57279350+Rohan1810@users.noreply.github.com>
2024-08-08 18:22:15 +0800

    update add.jsx

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,40 +1,47 @@
-import React, { useState } from 'react'
+import React from 'react'
+import {useEffect} from 'react'
+import {useState} from 'react'
+import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
 const Add = () => {
-    const [book, setBook] = useState({
-        title: "",
-        desc: "",
-        price: null,
-        cover: "",
-    })
+    const [user,setUser] = useState({
+        roleid:"",
+        nric:"",
+        fname:"",
+        lname:"",
+        contact:"",
+        email:""
+    });
 
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
-    const handleChange = (e) => {
-        setBook((prev) => ({...prev, [e.target.name]: e.target.value }));
+    const handleChange = (e) =>{
+        setUser(prev=>({...prev, [e.target.name]: e.target.value}))
     };
 
-    const handleClick = async (e) => {
+    const handleClick = async e => {
         e.preventDefault()
-        try {
-            await axios.post("http://localhost:8800/books", book)
+        try{
+            await axios.post("http://localhost:8800/users", user)
             navigate("/")
-        } catch (error) {
-            console.log(error);
+        }catch(err){
+            console.log(err)
         }
-    };
-    console.log(book);
-    return (
-        <div className="form">
-            <h1>add new book</h1>
-            <input type="text" placeholder='title' onChange={handleChange} name="title"/>
-            <input type="text" placeholder='desc' onChange={handleChange} name='desc'/>
-            <input type="number" placeholder='price' onChange={handleChange} name='price'/>
-            <input type="text" placeholder='cover' onChange={handleChange} name='cover'/>
+    }
 
-            <button onClick={handleClick}>add</button>
+    console.log(user)
+
+    return (
+        <div className='form'>
+            <h1>Particulars</h1>
+            <input type="text" placeholder='roleid' onChange={handleChange} name="roleid" />
+            <input type="text" placeholder='nric' onChange={handleChange} name="nric" />
+            <input type="text" placeholder='first name' onChange={handleChange} name="fname" />
+            <input type="text" placeholder='last name' onChange={handleChange} name="lname" />
+            <input type="text" placeholder='contact number' onChange={handleChange} name="contact" />
+            <input type="text" placeholder='email' onChange={handleChange} name="email" />
+            <button className="formButton" onClick={handleClick}>Add</button>
         </div>
     )
 }
