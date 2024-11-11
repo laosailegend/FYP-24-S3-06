130587fd79c2897c66a1128a8167a0bc1b259aed
laosailegend <threshfreak@gmail.com>
2024-11-12 02:48:54 +0800

    revert to lambda config

--- server/controller/logController.js
+++ server/controller/logController.js
@@ -5,16 +5,16 @@ const lambda = new aws.Lambda();
 const bucket = process.env.AWS_S3_BUCKET;
 
 // COMMENT OUT IN LAMBDA, MUST USE IN LOCALHOST
-const accessKey = process.env.AWS_ACCESS_KEY_ID;
-const secretKey = process.env.AWS_SECRET_ACCESS_KEY;
-const region = process.env.AWS_REGION;
+// const accessKey = process.env.AWS_ACCESS_KEY_ID;
+// const secretKey = process.env.AWS_SECRET_ACCESS_KEY;
+// const region = process.env.AWS_REGION;
 
 // AWS SDK configuration SAME AS ABOVE
-aws.config.update({
-    accessKeyId: accessKey,
-    secretAccessKey: secretKey,
-    region: region
-});
+// aws.config.update({
+//     accessKeyId: accessKey,
+//     secretAccessKey: secretKey,
+//     region: region
+// });
 
 // get logs
 exports.getLogs = (req, res) => {
